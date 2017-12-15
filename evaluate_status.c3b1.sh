10d50fe9f7cb1582753afae458f10cf97483069e
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2017-12-14 19:37:45 -0600

    Updating evaluate_status for CPTAC3 batch 1
    * merge of GDC_import and BRCA77 versions

--- evaluate_status.c3b1.sh
+++ evaluate_status.c3b1.sh
@@ -14,16 +14,7 @@
 # options
 # -f status: output only lines matching status, e.g., -f import:completed
 # -u: include only UUID in output
-# -D: include data file path in output
 
-# CPTAC3.b1 MGI data download location
-DATA_DIR="/gscmnt/gc2521/dinglab/mwyczalk/CPTAC3-download/"
-mkdir -p $DATA_DIR
+source CPTAC3.b1.paths.sh
 
-export IMPORTGDC_HOME="/gscuser/mwyczalk/src/importGDC"
-
-
-# Example 
-# evaluate_status.c3b1.sh -u -f import:ready dat/WXS.batch.dat 
-
-bash $IMPORTGDC_HOME/batch.import/evaluate_status.sh -O $DATA_DIR "$@"
+bash $SWW_HOME/src/evaluate_status.sh -D $DATAD_H -S $SCRIPTD_H "$@" $CASES
